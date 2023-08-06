1d2f7696865a824426ac42dee14a3db5005a29b8
allansmithll <allanammancio2004@gmail.com>
2023-08-05 22:46:50 -0300

    Revisão de JavaScript

---
+++ 01-revisao-javascript/teste01.js
@@ -0,0 +1,5 @@
+// Questão 1 da lista teórica de Javascript
+let idade = 20;
+//const nome;
+nome ="Maria";
+console.log(nome);
---
+++ 01-revisao-javascript/teste02.js
@@ -0,0 +1,9 @@
+// Questão 2 da revisão de JavaScript
+let x = 10;
+let y = 20;
+console.log(true);
+
+// Questão 8 de laços
+for (let i = 0; i<0;;; i++) {
+    console.log(i);
+}
